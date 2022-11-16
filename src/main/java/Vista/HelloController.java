98dd33d8e3fcbd3664e8f0ef5e98ce5a056729c7
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-11-16 14:58:13 -0600

    Conectar dos ventanas

--- src/main/java/Vista/HelloController.java
+++ src/main/java/Vista/HelloController.java
@@ -1,14 +1,30 @@
 package Vista;
 
+import Controlador.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+    public void onHelloButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RegistroCliente.fxml"));
+        Parent root = fxmlLoader.load();
+        RegistroClienteController controlador = fxmlLoader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
+
+    };
 }
