9ddc5f8729f5d08d490f42dabe2151e745fbdf31
cvsolsolcenfo <115435577+cvsolsolcenfo@users.noreply.github.com>
2022-11-16 15:42:54 -0600

    Conectar dos ventanas

--- src/main/java/Vista/HelloApplication.java
+++ src/main/java/Vista/HelloApplication.java
@@ -11,9 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("'" +
-                "'" +
-                "hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Banco Los Lavadores!");
         stage.setScene(scene);
